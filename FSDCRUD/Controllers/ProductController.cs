8b79bfa63d6c0da32ab4cb4597792b98f43de068
VidyaKinjarapu <vidya5594@gmail.com>
2025-08-21 10:18:25 -0400

    Adding angular and dotnet folders

---
+++ FSDCRUD/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using FSDCRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FSDCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+        public ProductController(ProductDbContext context) {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+                if (product == null)
+                    return NotFound();
+
+                return Ok(product);     
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(new { message = "Product inserted" });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(Product product, int id)
+        {
+            var editproduct = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (editproduct == null)
+                return NotFound();
+            editproduct.Name = product.Name;
+            editproduct.Price = product.Price;
+
+            await _context.SaveChangesAsync();
+            return Ok(new { message = "Product updated" });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var delproduct = await _context.Products.FirstOrDefaultAsync(y => y.Id == id);
+            if (delproduct == null)
+                return NotFound();
+            _context.Products.Remove(delproduct);
+            await _context.SaveChangesAsync();
+            return Ok(new { message = "Delete product" });
+        }
+    }
+}
---
+++ FSDCRUD/Models/ProductDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FSDCRUD.Models
+{
+    public class ProductDbContext:DbContext
+    {
+        public ProductDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
